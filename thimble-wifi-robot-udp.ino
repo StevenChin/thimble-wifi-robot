47420033082c9afb5cd9b8b638893d5ee767fb1b
StevenChin <chin.steven16@gmail.com>
2018-02-18 16:29:53 -0400

    include json parser to process commands, change auth strategy

--- thimble-wifi-robot-udp.ino
+++ thimble-wifi-robot-udp.ino
@@ -3,14 +3,85 @@
 #include "wifiConnection.h"
 #include "motorControl.h"
 
-char packetBuffer[255];          // buffer to hold incoming packet
-char ReplyBuffer[] = "ACK";      // a string to send back
+#define MAX_BUFFER_LEN 150
+
+StaticJsonBuffer<MAX_BUFFER_LEN> jsonBuffer;
 
 void setup() {
   // initialize serial for debugging
   Serial.begin(115200);
   wifiConnection::init();
   motorControl::init();
+}
+
+void handleMoveCmd(JsonObject& payload) {
+  bool validToken = userAuthentication::isTokenMatch(payload["authToken"]);
+
+  if (validToken) {
+    moveRobot(payload["data"]);
+  } else {
+    sendInvalidTokenResponse();
+  }
+}
+
+void moveRobot(JsonObject& payload) {
+  char *direction = payload["direction"];
+
+  if (strcmp(direction, "w") == 0) {
+    motorControl::goForward();
+  } else if (strcmp(direction, "a") == 0) {
+    motorControl::goLeft();
+  } else if (strcmp(direction, "s") == 0) {
+    motorControl::goBack();
+  } else if (strcmp(direction, "d") == 0) {
+    motorControl::goRight();
+  }
+}
+
+void sendInvalidCmdResponse() {
+  char *reason = "Command not supported";
+  sendResponse(createErrorResponse(reason));
+}
+
+void sendInvalidTokenResponse() {
+  char *reason = "Authentication token is invalid";
+  sendResponse(createErrorResponse(reason));
+}
+
+void sendParsingErrorResponse() {
+  char *reason = "Failed to parse payload, ensure valid json is sent";
+  sendResponse(createErrorResponse(reason));
+}
+
+JsonObject& createErrorResponse(char *reason) {
+  JsonObject& response = jsonBuffer.createObject();
+  response["status"] = "failed";
+  response["reason"] = reason;
+  return response;
+}
+
+void sendResponse(JsonObject& response) {
+  char replyBuffer[MAX_BUFFER_LEN];
+  response.printTo(Serial);
+  Serial.println();
+  response.printTo(replyBuffer, MAX_BUFFER_LEN);
+  wifiConnection::Udp.beginPacket(wifiConnection::Udp.remoteIP(), wifiConnection::Udp.remotePort());
+  wifiConnection::Udp.write(replyBuffer);
+  wifiConnection::Udp.endPacket();
+  memset(replyBuffer, 0, sizeof(replyBuffer));
+}
+
+void handleAuthCmd(JsonObject& payload) {
+  char *token = userAuthentication::initSession();
+  JsonObject& response = jsonBuffer.createObject();
+  response["status"] = "success";
+  JsonObject& data = response.createNestedObject("data");
+  data["authToken"] = token;
+  sendResponse(response);
+}
+
+void cleanup() {
+  jsonBuffer.clear();
 }
 
 void loop() {
@@ -24,52 +95,31 @@ void loop() {
     }
   }
 
-  // if there's data available, read a packet
   int packetSize = wifiConnection::Udp.parsePacket();
   if (packetSize) {
-    Serial.print("Received packet of size ");
-    Serial.println(packetSize);
-    Serial.print("From ");
-    IPAddress remoteIp = wifiConnection::Udp.remoteIP();
-    Serial.print(remoteIp);
-    Serial.print(", port ");
-    Serial.println(wifiConnection::Udp.remotePort());
-
+    char packetBuffer[MAX_BUFFER_LEN];
     // read the packet into packetBufffer
-    int len = wifiConnection::Udp.read(packetBuffer, 255);
+    int len = wifiConnection::Udp.read(packetBuffer, MAX_BUFFER_LEN);
     if (len > 0) {
       packetBuffer[len] = 0;
     }
-    Serial.println("Contents:");
-    Serial.println(packetBuffer);
 
-    // Use a switch statement to examine the first character of the packet buffer
-    switch (packetBuffer[0]) {
-      case 'w':
-        // If the value is 'w', move the robot forward
-        motorControl::goForward();
-        break;
-      case 'a':
-        // If the value is 'a', move the robot left
-        motorControl::goLeft();
-        break;
-      case 's':
-        // If the value is 's', move the robot backward
-        motorControl::goBack();
-        break;
-      case 'd':
-        // If the value is 's', move the robot right
-        motorControl::goRight();
-        break;
+    JsonObject& payload = jsonBuffer.parseObject(packetBuffer);
+    if (!payload.success()) {
+      sendParsingErrorResponse();
+    } else {
+      char *cmd = payload["command"];
+      if (strcmp(cmd, "move") == 0) {
+        handleMoveCmd(payload);
+      } else if (strcmp(cmd, "auth") == 0) {
+        handleAuthCmd(payload["data"]);
+      } else {
+        sendInvalidCmdResponse();
+      }
     }
 
-    // send a reply, to the IP address and port that sent us the packet we received
-    wifiConnection::Udp.beginPacket(wifiConnection::Udp.remoteIP(), wifiConnection::Udp.remotePort());
-    wifiConnection::Udp.write(ReplyBuffer);
-    wifiConnection::Udp.endPacket();
-
-    // Zero the packetBuffer first character until we receive a new packet
-    packetBuffer[0] = 0;
+    memset(packetBuffer, 0, sizeof(packetBuffer));
+    cleanup();
   }
 }
 
--- userAuthentication.h
+++ userAuthentication.h
@@ -1,95 +1,39 @@
-#include <sha256.h>
-
 #define TOKEN_LEN 16
-#define HASHED_PWD_LEN 64
+
+char *sessionToken;
 
 namespace userAuthentication {
-
-struct Credential {
-  char *username;
-  char *pwd;
-  char *token;
-};
-
-Credential creds = {
-  "<username>",
-  "<hashed pwd>"
-};
-
-const char alphanumeric_table[62] = {
-  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-};
-
-void printHash(uint8_t *hash) {
-  for (int i = 0; i < 32; i++) {
-    Serial.print("0123456789abcdef"[hash[i] >> 4]);
-    Serial.print("0123456789abcdef"[hash[i] & 0xf]);
-  }
-
-  Serial.println();
-}
-
-void convertHashToString(uint8_t *hash, char *hashStr) {
-  int hashIndex = 0;
-
-  for (int i = 0; i < 32; i++) {
-    hashStr[hashIndex] = "0123456789abcdef"[hash[i] >> 4];
-    hashIndex++;
-    hashStr[hashIndex] = "0123456789abcdef"[hash[i] & 0xf];
-    hashIndex++;
-  }
-
-  // null terminate array
-  hashStr[hashIndex] = '\0';
-}
-
-void hashPassword(char *pwd,  char *hashedPwd) {
-  uint8_t *hash;
-  Sha256 sha256;
-  sha256.init();
-  sha256.print(pwd);
-  hash = sha256.result();
-  convertHashToString(hash, hashedPwd);
-}
 
 char *generateToken() {
   // randomize sequence for tokens becuase they should not be guessable!
   randomSeed(millis());
 
   char *token = new char[TOKEN_LEN + 1];
-  int randMax = sizeof(alphanumeric_table);
+  int randMax = 10;
+  int n;
 
   for (int i = 0; i < TOKEN_LEN; i++) {
-    token[i] = alphanumeric_table[random(0, randMax)];
+    // convert random numbers to their char form using ASCII code
+    token[i] = ( (char) (random(0, randMax) + '0') );
   }
 
   token[TOKEN_LEN] = '\0';
   return token;
 }
 
-char *authenticate (char *username, char *pwd) {
-  char *token;
-  char *hashedPwd = new char[HASHED_PWD_LEN + 1];
-  hashPassword(pwd, hashedPwd);
-
-  if ( strcmp(username, creds.username) == 0 && strcmp(hashedPwd, creds.pwd) == 0) {
-    delete [] creds.token;
-    creds.token = generateToken();
-    token = creds.token;
-  } else {
-    delete [] token;
+char *initSession () {
+  if (!sessionToken || sessionToken[0] == '\0') {
+    delete [] sessionToken;
   }
-
-  delete [] hashedPwd;
-  return token;
+  sessionToken = generateToken();
+  return sessionToken;
 }
 
 bool isTokenMatch (char *token) {
-  return strcmp(token, creds.token) == 0;
+  if (!token || token[0] == '\0') {
+    return false;
+  }
+  return (strcmp(token, sessionToken) == 0);
 }
 
 }
